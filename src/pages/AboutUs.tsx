3e7d2ac24e12947f549af20b7e6b56057135fd2c
Persik <lia.griscenco@isa.utm.md>
2026-03-31 11:10:18 +0300

    Textblock aboutUs

--- src/pages/AboutUs.tsx
+++ src/pages/AboutUs.tsx
@@ -21,6 +21,21 @@ export default function AboutUs() {
           <button className="about-hero__button">View catalog</button>
         </div>
       </section>
+      <section className="about-intro">
+  <div className="about-intro__container">
+    <h2 className="about-intro__title">About FloraShop</h2>
+
+    <p className="about-intro__text">
+      FloraShop was created for people who love natural beauty, soft interiors,
+      and elegant details.
+    </p>
+
+    <p className="about-intro__text">
+      We carefully select flowers, plants, and decor that make your home feel
+      calm, fresh, and full of life.
+    </p>
+  </div>
+</section>
     </main>
   );
 }
